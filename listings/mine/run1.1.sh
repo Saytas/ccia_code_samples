b48187184ed26fe573a2e047f2595498b3ab5bfe
sela <sela@selas-mbp.lan>
2022-06-11 17:14:00 -0700

    <thread> lib

---
+++ listings/mine/run1.1.sh
@@ -0,0 +1,2 @@
+g++ -std=c++20 -g -Wall -Wextra -Iinclude -fno-exceptions -pthread -o listing_1.1 listing_1.1.cpp 
+#g++ -std=c++20 -g -Llib -Wall -Wextra -Iinclude -fno-exceptions -pthread -o listing_1.1 listing_1.1.cpp 
